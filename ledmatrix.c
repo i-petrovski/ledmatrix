4a309409a0a793c360b310dc69509090e17b32c6
i-petrovski <ipetrovski81@gmail.com>
2018-12-12 17:52:35 -0500

    add sprites

--- ledmatrix.c
+++ ledmatrix.c
@@ -1,5 +1,5 @@
 #include "ledmatrix.h"
-//#include "sprite.h"
+#include "sprite.h"
 #include <util/delay.h>
 
 // Bit
@@ -18,107 +18,75 @@ void pointtest (void)
   // light one LED at a time, scanning left to right
   // and top to bottom... useful for testing the matrix
   //32
-    for (x=0; x<13; x++) 
+    for (x=0; x<32; x++) 
     {
       for (y=0; y<8; y++)
       {
         write(x, y, HIGH);
-        //_delay_ms(20);
-        //write(x, y, LOW);
+        _delay_ms(20);
+        write(x, y, LOW);
       } 
     }
-  }
-}
-
-void setBox(uint8_t SETBIT)
-{
-  uint8_t x=0, y=0;
-    for (y=0; y<7; ++y)
-    {
-      write(x,y,SETBIT);
-      _delay_ms(speed);
-    }
-
-    for (x=0; x<7; ++x)
-    {
-      write(x,y,SETBIT);
-      _delay_ms(speed);
-    }
-
-    for (y=7; y>0; --y)
-    {
-      write(x,y,SETBIT);
-      _delay_ms(speed);
-    }
-
-    for (x=7; x>0; --x)
-    {
-      write(x,y,SETBIT);
-      _delay_ms(speed);
-    }
-}
-
-void regTest(uint8_t REGBYTE)
-{
-  setRegister(0x01,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x02,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x03,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x04,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x05,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x06,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x07,REGBYTE);
-  _delay_ms(speed);
-  setRegister(0x08,REGBYTE);
-  _delay_ms(speed);
-}
-
-void loadXY(void)
-{
-  uint8_t y=0, x=0;
-
-  while (y<8)
-  {
-    write(0, y, HIGH);
-    _delay_ms(speed);
-    y++;
-  }
-  while (x<8)
-  {
-    write(x, 0, HIGH);
-    _delay_ms(speed);
-    x++;
   }
 }
 
 int main(void)
 {
   setup();
+  uint8_t letter_L[] = {5, 8,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11111,
+    0b11111
+  };
+  
+  uint8_t letter_E[] = {5, 8,
+    0b11111,
+    0b11111,
+    0b11000,
+    0b11110,
+    0b11110,
+    0b11000,
+    0b11111,
+    0b11111
+  };
 
-  //swrite(0, 0, letter_L)
+  uint8_t letter_D[] = {6, 8,
+    0b111100,
+    0b111110,
+    0b110111,
+    0b110011,
+    0b110011,
+    0b110111,
+    0b111110,
+    0b111100
+  };
 
-  //loadXY();
-  pointtest();
+  uint8_t letter_S[] = {5, 8,
+    0b01110,
+    0b11111,
+    0b11000,
+    0b11110,
+    0b01111,
+    0b00011,
+    0b11111,
+    0b01110
+  };
+  
+  spritewrite(0,letter_L);
+  spritewrite(6,letter_E);
+  spritewrite(12,letter_D);
+  spritewrite(19,letter_S);
+
+  //pointtest();
   //regTest(0xff);
-
-  //setBox(HIGH);
-  //setBox(LOW);
-  //_delay_ms(500);
-  //write(0,0,HIGH);
-
-  //setRegister(0x08,0b11111111);
-  //uint8_t x=0, y=0;
   //write(0,0,HIGH);
   //_delay_ms(1000);
   //write(0,0,LOW);
-  //while(1){
-  //  setBox(HIGH);
-  //  setBox(LOW);
-  //}
+  //setRegister(0x08,0b11111111);
   return 0;
 }
--- sprite.h
+++ sprite.h
@@ -1,75 +1,24 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
+#define HIGH 1
+#define LOW 0
 
-int _buffer [0];
-//uint8_t _buffer [0] = {0};
-uint8_t _width;
-uint8_t _height;
-
-
-void init(uint8_t width, uint8_t height)
-{
-  _width = width >= 8 ? 8 : width;
-  _height = height >= 8 ? 8 : height;
-
-  // for now, do nothing if this allocation fails.  methods that require it
-  // should silently fail if _buffer is null.
-  //_buffer = (uint8_t *) calloc(_height, 1);
-}
-
-void swrite(uint8_t x, uint8_t y, uint8_t value)
-{
-  //if (!_buffer) return;
-  
-  // uint8_t's can't be negative, so don't test for negative x and y.
-  if (x >= _width || y >= _height) return;
-  
-  // we need to bitwise-or the value of the other pixels in the byte with
-  // the new value, masked and shifted into the proper bits.
-  _buffer[y] = (_buffer[y] & ~(0x01 << x)) | ((value & 0x01) << x);
-}
-
-void Sprite(uint8_t width, uint8_t height, uint8_t row, ...)
-{
-  init(width, height);
-  
-  //if (!_buffer) return;
-  
-  va_list ap;
-  va_start(ap, row);
-
-  int y = 0;
-  for (y = 0; ; y++) {
-    int x;
-    for (x = 0; x < width && x < 8; x++)
-      swrite(x, y, (row >> (width - x - 1)) & 0x01);
-    
-    if (y == height - 1)
-      break;
-      
-    row = va_arg(ap, int); // using '...' promotes uint8_t to int
+void spritex(uint8_t startx, uint8_t starty, uint8_t width, uint8_t bits){
+  startx = startx + width - 1;
+  while (bits) {
+      if (bits & 1) {
+          write(startx,starty,HIGH);
+      } else {
+          write(startx,starty,LOW);
+      }
+      startx--;
+      bits >>= 1;
   }
-  
-  va_end(ap);
 }
 
-const uint8_t width()
-{
-  return _width;
-}
-
-const uint8_t height()
-{
-  return _height;
-}
-
-const uint8_t read(uint8_t x, uint8_t y)
-{
-  if (!_buffer) return 0;
-  
-  // uint8_t's can't be negative, so don't test for negative x and y.
-  if (x >= _width || y >= _height) return 0;
-  
-  return (_buffer[y] >> x) & 0x01;
+void spritewrite(uint8_t posx, uint8_t sprite[]){
+  uint8_t spritey, posy;
+  posy = sprite[1] - 1;
+  for (spritey=2; spritey<sprite[1] + 2; spritey++){
+    spritex(posx,posy,sprite[0],sprite[spritey]);
+    posy--;
+  }
 }

20b2cd05e77087f5d8dd07c07b3a587595ed220c
Igor Petrovski <igor.petrovski@Igors-MacBook-Pro-2.local>
2025-03-12 15:13:16 -0400

    added buttions

--- ledmatrix.c
+++ ledmatrix.c
@@ -7,6 +7,11 @@
 #define LOW 0
 #define speed 10
 
+#define BUTTON_PIN_F0  PF0   // Input button on Pin F0
+#define OUTPUT_PIN_F1  PF1  // Output on Pin F1
+#define BUTTON_PIN_F4  PF4   // Input button on Pin F2
+#define OUTPUT_PIN_F5  PF5  // Output on Pin F3
+
 void pointtest (void)
 {
   uint8_t x, y;
@@ -30,10 +35,53 @@ void pointtest (void)
   }
 }
 
+
+void button_setup (void)
+{
+  // Button 1
+  DDRF &= ~(1 << BUTTON_PIN_F0);  // Set F0 as input
+  PORTF |= (1 << BUTTON_PIN_F0);  // Enable pull-up resistor
+
+  DDRF |= (1 << OUTPUT_PIN_F1);   // Set F1 as output
+  PORTF &= ~(1 << OUTPUT_PIN_F1); // Ensure output starts LOW
+
+  // Button 2
+  DDRF &= ~(1 << BUTTON_PIN_F4);  // Set F0 as input
+  PORTF |= (1 << BUTTON_PIN_F4);  // Enable pull-up resistor
+  
+  DDRF |= (1 << OUTPUT_PIN_F5);   // Set F1 as output
+  PORTF &= ~(1 << OUTPUT_PIN_F5); // Ensure output starts LOW
+}
+
 int main(void)
 {
   setup();
+  button_setup();
+  write(7, 7, HIGH);
   
+  while (1) {
+    if (!(PINF & (1 << BUTTON_PIN_F0))) 
+    {  // Button pressed (active low)
+        //PORTF ^= (1 << OUTPUT_PIN);     // Toggle output
+        write(0, 0, HIGH);
+        _delay_ms(200);                 // Debounce delay
+    }
+    else
+    {
+      write(0, 0, LOW);
+    }
+    if (!(PINF & (1 << BUTTON_PIN_F4))) 
+    {  // Button pressed (active low)
+        //PORTF ^= (1 << OUTPUT_PIN);     // Toggle output
+        write(1, 0, HIGH);
+        _delay_ms(200);                 // Debounce delay
+    }
+    else
+    {
+      write(1, 0, LOW);
+    }
+  }
+
   /*
   spritewrite(0,letter_C);
   spritewrite(6,letter_L);
@@ -42,24 +90,23 @@ int main(void)
   spritewrite(26,letter_D);
   */
   
+  /* 
   uint8_t movex;
   movex = 0;
-  while(200)
-  {  
-    spritewrite(0 + movex,letter_C);
-    spritewrite(6 + movex,letter_L);
-    spritewrite(12 + movex,letter_O);
-    spritewrite(19 + movex,letter_U);
-    spritewrite(26 + movex,letter_D);
+  while(1)
+  {
+    //spritewrite(0 + movex,letter_TST);
+    spritemove(0 + movex, 1,letter_TST);
     _delay_ms(100);
-    clearScreen();
+    //clearScreen();
     movex++;
-    if (movex > 32) {
+    if (movex > 32) 
+    {
       movex = 0;
     }
   }
+  */
   
-
   //pointtest();
   //regTest(0xff);
   //write(0,0,HIGH);
